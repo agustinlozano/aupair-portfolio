0a87ee8a72e450fbddb672cb5842a4ee54bac774
Agustin B. Lozano <agustinlozanoblua@gmail.com>
2023-03-03 17:33:56 -0300

    Add main css and main jsx

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import Header from "./components/Header"
+import Welcome from "./components/Welcome"
+import ExperienceContainer from "./components/Experience"
+import { motion, useScroll } from 'framer-motion'
+import { LanguageContext } from './context/multilingual'
+import { useState } from "react"
+import './app.css'
+
+function App() {
+  const { scrollYProgress } = useScroll()
+  const [language, setLanguage] = useState('spanish')
+
+  const toggleLanguage = () => {
+    setLanguage((language) => (language === 'spanish' ? 'english' : 'spanish'))
+  }
+
+  return (
+    <>
+      <LanguageContext.Provider value={{ language, toggleLanguage }}>
+        <motion.div className="progress-bar" style={{ scaleX: scrollYProgress, zIndex: 1000 }} />
+        <Header />
+        <div className="m-auto">
+          <Welcome />
+          <ExperienceContainer />
+        </div>
+      </LanguageContext.Provider>
+    </>
+  )
+}
+
+export default App
