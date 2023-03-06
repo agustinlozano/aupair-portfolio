aa288af9a23f93a14a6c1529faa86385817ff569
Agustin B. Lozano <agustinlozanoblua@gmail.com>
2023-03-06 18:24:04 -0300

    Wrap the logo with an anchor

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,8 +9,9 @@ const Header = () => {
     <div className="w-100 px-7 py-4 sticky top-0 z-50 bg-slate-100 border-b-2 shadow-sm mb-5 md:px-20">
       <div className="flex justify-between items-center">
         <div className="flex items-center">
-          <Svg svgFile={logo} style="w-14" />
-          {/* <p className="text-violet-500 mx-4 text-4xl font-bold">Au Pair</p> */}
+          <a href='/#root'>
+            <Svg svgFile={logo} style="w-14" />
+          </a>
         </div>
         <div className="flex items-center">
           <div>
