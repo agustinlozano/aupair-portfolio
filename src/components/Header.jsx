d1d4f328a9ae41fb9247541c30b23d05f85696fb
Agustin B. Lozano <agustinlozanoblua@gmail.com>
2023-03-17 20:13:29 -0300

    Improve responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,17 +6,16 @@ import DropdownMenu from './DropdownMenu'
 // Create a sticky header component that will be used in the App component.
 const Header = () => {
   return (
-    <div className="w-100 px-7 py-4 sticky top-0 z-50 bg-slate-100 border-b-2 shadow-sm mb-5 md:px-20">
-      <div className="flex justify-between items-center">
-        <div className="flex items-center">
+    <div className="w-100 py-4 sticky top-0 z-50 bg-slate-100 border-b-2 shadow-sm mb-5">
+      <div className="flex justify-between items-center max-w-screen-2xl m-auto">
+        <div className="flex items-center mx-20 max-sm:mx-3">
           <a href='/#root'>
             <Svg svgFile={logo} style="w-14" />
           </a>
         </div>
-        <div className="flex items-center">
-          <div>
-            <LanguageButton />
-          </div>
+        {/* <div className="flex items-center w-fit md:mr-12 lg:mr-12 xl:mr-12"> */}
+        <div className="flex items-center w-fit mx-20 max-sm:mx-3">
+          <LanguageButton />
           <DropdownMenu />
         </div>
       </div>
