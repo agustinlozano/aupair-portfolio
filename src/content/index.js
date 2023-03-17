819aff5a93fc1d6cbc5391633cb92e4dd1ac9944
Agustin B. Lozano <agustinlozanoblua@gmail.com>
2023-03-17 20:15:26 -0300

    Move all the text content variables into a file

---
+++ src/content/index.js
@@ -0,0 +1,111 @@
+export const welcomeCnt = {
+  english: {
+    greeting: "Hi! I'm",
+    welcome: "I'd like to invite you to take a look at my"
+  },
+  spanish: {
+    greeting: "Hola! Soy",
+    welcome: "Te invito a conocer mi"
+  }
+}
+
+export const experienceCnt = {
+  english: {
+    title: "EXPERIENCE",
+    items: [
+      {
+        id: 1,
+        title: "MY AMAZING EXPERIENCE WITH BRUNELLA 👧🏼",
+        content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum repudiandae exercitationem veritatis sapiente sequi molestiae doloribus quos earum ipsum voluptatibus ratione incidunt, mollitia sint explicabo nulla odit optio molestias. Nesciunt non rem voluptate! Assumenda officia laudantium praesentium, nihil sit ab dolor reiciendis quam sequi omnis! Maiores esse laboriosam soluta tempora",
+        date: "2020 - 2021",
+        location: {
+          city: "Rosario, Arg",
+          coords: "https://goo.gl/maps/FRLYzVraMFjHSoeP8"
+        },
+        labels: ["part-time", "one-child", "spanish", "supervision", "bath"]
+      },
+      {
+        id: 2,
+        title: "VICKTORIA THE LITTLE PRINCESS 👶🏽",
+        content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum repudiandae exercitationem veritatis sapiente sequi molestiae doloribus quos earum ipsum voluptatibus ratione incidunt, mollitia sint explicabo nulla odit optio molestias. Nesciunt non rem voluptate! Assumenda officia laudantium praesentium, nihil sit ab dolor reiciendis quam sequi omnis! Maiores esse laboriosam soluta tempora",
+        date: "2019 - 2020",
+        location: {
+          city: "Pergamino, Arg",
+          coords: "https://goo.gl/maps/MyYvEfCR4ZG4Zm3c8"
+        },
+        labels: ["one-child", "spanish", "infant", "diapering", "entertainment" ]
+      },
+      {
+        id: 3,
+        title: "TAKING CARE OF LUCAS 👦🏻",
+        content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum repudiandae exercitationem veritatis sapiente sequi molestiae doloribus quos earum ipsum voluptatibus ratione incidunt, mollitia sint explicabo nulla odit optio molestias. Nesciunt non rem voluptate! Assumenda officia laudantium praesentium, nihil sit ab dolor reiciendis quam sequi omnis! Maiores esse laboriosam soluta tempora",
+        date: "2018 - 2019",
+        location: {
+          city: "Pergamino, Arg",
+          coords: "https://goo.gl/maps/MyYvEfCR4ZG4Zm3c8"
+        },
+        labels: ["spanish", "10-years-old", "meal", "homework", "transportation"]
+      },
+      {
+        id: 4,
+        title: "I looked after afro-american kids as well 👦🏽",
+        content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum repudiandae exercitationem veritatis sapiente sequi molestiae doloribus quos earum ipsum voluptatibus ratione incidunt, mollitia sint explicabo nulla odit optio molestias. Nesciunt non rem voluptate! Assumenda officia laudantium praesentium, nihil sit ab dolor reiciendis quam sequi omnis! Maiores esse laboriosam soluta tempora",
+        date: "2018 - 2019",
+        location: {
+          city: "Pergamino, Arg",
+          coords: "https://goo.gl/maps/MyYvEfCR4ZG4Zm3c8"
+        },
+        labels: ["12-years-old", "meal", "supervision"]
+      }
+    ]
+  },
+  spanish: {
+    title: "EXPERIENCIA",
+    items: [
+      {
+        id: 1,
+        title: "MI FANTASTICA EXPERIENCIA CON BRUNELLA 👧🏼",
+        content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum repudiandae exercitationem veritatis sapiente sequi molestiae doloribus quos earum ipsum voluptatibus ratione incidunt, mollitia sint explicabo nulla odit optio molestias. Nesciunt non rem voluptate! Assumenda officia laudantium praesentium, nihil sit ab dolor reiciendis quam sequi omnis! Maiores esse laboriosam soluta tempora",
+        date: "2020 - 2021",
+        location: {
+          city: "Rosario, Arg",
+          coords: "https://goo.gl/maps/FRLYzVraMFjHSoeP8"
+        },
+        labels: ["part-time", "one-child", "supervision", "bath"]
+      },
+      {
+        id: 2,
+        title: "Vicktoria la pequeña princesa 👶🏽",
+        content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum repudiandae exercitationem veritatis sapiente sequi molestiae doloribus quos earum ipsum voluptatibus ratione incidunt, mollitia sint explicabo nulla odit optio molestias. Nesciunt non rem voluptate! Assumenda officia laudantium praesentium, nihil sit ab dolor reiciendis quam sequi omnis! Maiores esse laboriosam soluta tempora",
+        date: "2019 - 2020",
+        location: {
+          city: "Pegamino, Arg",
+          coords: "https://goo.gl/maps/MyYvEfCR4ZG4Zm3c8"
+        },
+        labels: ["one-child", "infant", "diapering", "entertainment" ]
+      },
+      {
+        id: 3,
+        title: "Cuidando a Lucas 👦🏻",
+        content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum repudiandae exercitationem veritatis sapiente sequi molestiae doloribus quos earum ipsum voluptatibus ratione incidunt, mollitia sint explicabo nulla odit optio molestias. Nesciunt non rem voluptate! Assumenda officia laudantium praesentium, nihil sit ab dolor reiciendis quam sequi omnis! Maiores esse laboriosam soluta tempora",
+        date: "2018 - 2019",
+        location: {
+          city: "Pegamino, Arg",
+          coords: "https://goo.gl/maps/MyYvEfCR4ZG4Zm3c8"
+        },
+        labels: ["10-years-old", "meal", "homework", "transportation"]
+      },
+      {
+        id: 4,
+        title: "También cuidé niños afroamericanos 👦🏽",
+        content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum repudiandae exercitationem veritatis sapiente sequi molestiae doloribus quos earum ipsum voluptatibus ratione incidunt, mollitia sint explicabo nulla odit optio molestias. Nesciunt non rem voluptate! Assumenda officia laudantium praesentium, nihil sit ab dolor reiciendis quam sequi omnis! Maiores esse laboriosam soluta tempora",
+        date: "2018 - 2019",
+        location: {
+          city: "Pergamino, Arg",
+          coords: "https://goo.gl/maps/MyYvEfCR4ZG4Zm3c8"
+        },
+        labels: ["12-years-old", "meal", "supervision"]
+      }
+    ]
+  }
+}
