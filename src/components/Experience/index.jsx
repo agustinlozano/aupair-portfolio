3cb0cb082b4afd5a0b768c279dea96856887202c
Agustin B. Lozano <agustinlozanoblua@gmail.com>
2023-03-17 20:14:58 -0300

    Update Experience section

--- src/components/Experience/index.jsx
+++ src/components/Experience/index.jsx
@@ -1,8 +1,10 @@
-import { Parap, Subtitle, TextMd } from "../text"
-import { experiences } from "../../content/"
+import { Parap, Subtitle, TextMd, TextSm } from "../text"
+import { experienceCnt } from "../../content/"
 import { shuffleArray } from "../../utils/random"
 import Linker from "../atoms/Linker"
 import Label from "../atoms/Label"
+import { useContext } from "react"
+import { LanguageContext } from "../../context/multilingual"
 
 // an array of tailwind colors
 const colors = [
@@ -13,7 +15,13 @@ const colors = [
   "#f6f4d2",
 ]
 
-const Experience = ({children, title, labels}) => {
+const LocationPinSvg = () => (
+  <svg className="inline w-4 h-4 mb-1" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M12 2C8.13 2 5 5.13 5 9C5 14.25 12 22 12 22C12 22 19 14.25 19 9C19 5.13 15.87 2 12 2ZM12 13C10.34 13 9 11.66 9 10C9 8.34 10.34 7 12 7C13.66 7 15 8.34 15 10C15 11.66 13.66 13 12 13Z" fill="#374151" />
+  </svg>
+)
+
+const Experience = ({children, title, date, location, labels}) => {
   const randomColors = shuffleArray(colors)
 
   const randomLabels = labels.map((label, index) => {
@@ -26,7 +34,9 @@ const Experience = ({children, title, labels}) => {
   return (
     <div className="grid grid-cols-1 mb-20 gap-4 lg:m-20 lg:grid-cols-8">
       <div className="px-5 lg:col-span-5">
-        <TextMd color='dark'>{title}</TextMd>
+        <TextMd color='dark' className="text-center">{title}</TextMd>
+        <TextSm color='dark' className="mb-2">{date} | 
+        <a href={location.coords} className="underline hover:text-cyan-500" target="_blank"><LocationPinSvg />{location.city}</a>.</TextSm>
         <Parap>
           {children}
         </Parap>
@@ -45,16 +55,20 @@ const Experience = ({children, title, labels}) => {
 }
 
 const ExperienceContainer = () => {
+  const { language } = useContext(LanguageContext)
+
   return (
     <div id="experience" className="my-32 p-2 border-t sm:p-28 lg:p-6 lg:grid-cols-5 lg:mb-10 lg:mt-28 lg:mx-16">
       <Subtitle>
         <Linker id="#experience" />
-        EXPERIENCE
+        {experienceCnt[language].title}
       </Subtitle>
-      {experiences.map((experience) => (
+      {experienceCnt[language].items.map((experience) => (
         <Experience
           key={experience.id}
-          title={experience.title}
+          title={experience.title.toUpperCase()}
+          date={experience.date}
+          location={experience.location}
           labels={experience.labels}
         >
           {experience.content}
