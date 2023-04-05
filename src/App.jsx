8b1b8181bd02c85ec3daffb7f0c2ca874f9bd153
Agustin B. Lozano <agustinlozanoblua@gmail.com>
2023-04-04 23:20:44 -0300

    Update app

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import Header from "./components/Header"
-import Welcome from "./components/welcome/"
-import ExperienceContainer from "./components/Experience/"
 import { motion, useScroll } from 'framer-motion'
 import { LanguageContext } from './context/multilingual'
 import { useState } from "react"
-import './app.css'
+import ExperienceContainer from "./components/Experience"
+import Welcome from "./components/welcome"
+import AboutMe from "./components/AboutMe"
+import Header from "./components/Header"
 import GoUp from "./components/atoms/GoUp"
+import './app.css'
 
 function App() {
   const { scrollYProgress } = useScroll()
-  const [language, setLanguage] = useState('spanish')
+  const [language, setLanguage] = useState('english')
 
   const toggleLanguage = () => {
     setLanguage((language) => (language === 'spanish' ? 'english' : 'spanish'))
@@ -20,9 +21,10 @@ function App() {
       <LanguageContext.Provider value={{ language, toggleLanguage }}>
         <motion.div className="progress-bar" style={{ scaleX: scrollYProgress, zIndex: 1000 }} />
         <Header />
-        <div className="m-auto">
+        <div className="m-auto max-w-screen-2xl">
           <Welcome />
           <ExperienceContainer />
+          <AboutMe />
         </div>
         <GoUp />
       </LanguageContext.Provider>
