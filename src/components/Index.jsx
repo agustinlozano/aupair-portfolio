21d69dd7a1f3990cb4fd0ca7f4e9b99afa7a5e96
Agustin B. Lozano <agustinlozanoblua@gmail.com>
2023-04-06 16:53:15 -0300

    Create an index component

---
+++ src/components/Index.jsx
@@ -0,0 +1,17 @@
+import Welcome from "./Welcome"
+import AboutMe from "./AboutMe"
+import Contact from "./Contact"
+import ExperienceContainer from "./Experience"
+
+const Index = () => {
+  return (
+    <>
+      <Welcome />
+      <ExperienceContainer />
+      <AboutMe />
+      <Contact />
+    </>
+  )
+}
+
+export default Index
